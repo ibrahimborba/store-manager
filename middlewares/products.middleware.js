b61387c5a11e1c6f8b345917cb8d0575340d09b0
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-08-15 00:34:40 -0300

    feat: add post /porducts endpoint req.body validation middleware

---
+++ middlewares/products.middleware.js
@@ -0,0 +1,21 @@
+const joi = require('joi');
+const errors = require('../errors/customErrors');
+
+const productSchema = joi.object({
+  name: joi.string().min(5).required().messages({
+    'any.required': '400|"name" is required',
+    'string.min': '422|"name" length must be at least {#limit} characters long',
+  }),
+});
+
+const validator = (req, _res) => {
+  const product = { ...req.body };
+  const { error } = productSchema.validate(product);
+
+  if (error) {
+    const [status, message] = error.message.split('|');
+    return errors.customError(status, message);
+  }
+};
+
+module.exports = { validator };
--- routes/products.route.js
+++ routes/products.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const productsController = require('../controllers/products.controller');
+const productMiddleware = require('../middlewares/products.middleware');
 
 const router = express.Router();
 
 router.route('/')
   .get(productsController.getAll)
-  .post(productsController.add);
+  .post(productMiddleware.validator, productsController.add);
 
 router.get('/:id', productsController.getByPK);
 
