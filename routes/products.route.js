80c41af51de9c44e5061534fb71f5d5896c097ed
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-08-15 01:28:39 -0300

    style: change productsMiddleware import name

--- routes/products.route.js
+++ routes/products.route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const productsController = require('../controllers/products.controller');
-const productMiddleware = require('../middlewares/products.middleware');
+const productsMiddleware = require('../middlewares/products.middleware');
 
 const router = express.Router();
 
 router.route('/')
   .get(productsController.getAll)
-  .post(productMiddleware.validator, productsController.add);
+  .post(productsMiddleware.validator, productsController.add);
 
 router.get('/:id', productsController.getByPK);
 
